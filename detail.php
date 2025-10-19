cd7157ccf0286e4854791586ff32e07a6d607744
choesu <sunhak.choe@gmail.com>
2025-10-19 04:04:39 +0000

    Update from week 19

---
+++ detail.php
@@ -0,0 +1,33 @@
+<?php
+require('vendor/autoload.php');
+
+use Sc\Webproject\App;
+use Sc\Webproject\Book;
+
+// create an app object based on App class
+$app = new App();
+
+$title = "Detail Page";
+$message = "Hello there";
+
+// create an instance of the book class
+$cls_book = new Book();
+
+if (isset($_GET["id"])) {
+    //echo "<h2>" . $_GET["id"] . "</h2>";
+    // get the book details using its id
+    $book_detail = $cls_book->getBookById($_GET["id"]);
+    // print_r($book_detail);
+    // create a template loader
+    $loader = new \Twig\Loader\FilesystemLoader('templates');
+    $twig = new \Twig\Environment($loader);
+    // load the template into memory
+    $template = $twig->load('detail.html.twig');
+    // add some variables for twig to render
+    echo $template->render([
+        'title' => $book_detail["Title"],
+        'book' => $book_detail
+    ]);
+} else {
+    header("location: index.php");
+}
