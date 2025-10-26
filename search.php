68b6f4189262a99a4ef4fa137af1d1336783f26a
choesu <sunhak.choe@gmail.com>
2025-10-26 00:19:50 +0000

    added search class and page

--- search.php
+++ search.php
@@ -7,16 +7,16 @@ use Sc\Webproject\Search;
 // create an app object based on App class
 $app = new App();
 
-if( isset($_GET["query"])) {
-    $keyword = $_GET["query"];
-    // initialise search class
-    $serch = new Search();
-    // call getResults method and pass user keyword
-    $results = $search -> getResults($keyword);
-    foreach( $results as $item) {
-        print_r($item);
-        echo "<br>";
-    }
+if( isset($_GET["query"]) ) {
+   $keyword = $_GET["query"];
+   // initialise search class
+   $search = new Search();
+   // call getResults method and pass user keyword
+   $results = $search -> getResults($keyword);
+   foreach( $results as $item ) {
+    print_r($item);
+    echo "<br>";
+   }
 }
 else {
     echo "You are not searching";
