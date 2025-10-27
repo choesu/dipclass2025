e0d6ff6aae9afe9998b60e3eaa95639545b8c124
choesu <sunhak.choe@gmail.com>
2025-10-27 01:33:34 +0000

    adding account class for signup

---
+++ src/Account.php
@@ -0,0 +1,48 @@
+<?php
+namespace Sc\Webproject;
+
+use Exception;
+use Sc\Webproject\Database;
+
+class Account extends Database {
+    public function __construct()
+    {
+        // initialise the database connection
+        parent::__construct();       
+    }
+
+    public function create(
+        $email,
+        $password,
+        $username,
+        $first,
+        $last
+        )
+    {
+        $query = "
+        INSERT INTO Account
+        (Email,Password,Username,First,Last,Type)
+        VALUES
+        (?,?,?,?,?,1)
+        ";
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $statement = $this -> connection -> prepare($query);
+        $statement -> bind_param("sssss",$email,$hash,$username,$first,$last);
+        try {
+            if( $statement -> execute()) {
+                throw new Exception("Oops! Something went wrong");
+            }
+            else {
+                // account is created
+                return true;
+            }
+        }
+        catch( Exception $e) {
+            // do something
+            echo $e -> getMessage();
+            // failure to create account
+            return false;
+        }
+    }
+}
+?>
