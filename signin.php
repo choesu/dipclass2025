c00ac0e456e60fbb8540d8c6e62733c74c221f32
choesu <sunhak.choe@gmail.com>
2025-11-28 03:15:18 +0000

    added staffdashboard and userdashboard

--- signin.php
+++ signin.php
@@ -7,10 +7,13 @@ use Sc\Webproject\Account;
 // create an app object based on App class
 $app = new App();
 
-$title = "Sign up to UniLibrary";
-$message = "Join our website";
+$title = "Sign in to UniLibrary";
+$message = "Sign in to your account";
 $success = null;
-if( empty($_SESSION["username"])) {
+$response = null;
+$type = null;
+
+if( empty($_SESSION["username"]) ) {
     $user = null;
 }
 else {
@@ -19,25 +22,30 @@ else {
 
 // handle POST request from the signup form
 if( $_SERVER["REQUEST_METHOD"] == "POST" ) {
-    $username = $_POST["username"];
     $email = $_POST["email"];
-    $password1 = $_POST["password"];
-    // $password2 = $_POST["confirm-password"];
-    // initialise Account class
+    $password = $_POST["password"];
+    // authenticate user
     $account = new Account();
-    $signup = $account -> create($email,$password1,$username,"Test","User");
-    // check if signup is successful
-    if( $signup == true ) {
-        // successful
+    $signin = $account -> login($email,$password);
+    // check if signin is successful
+    if( $signin["success"] == true ) {
+        // success
         $success = true;
+        $username = $signin["account"]["Username"];
+        $type = $signin["account"]["Type"];
+
         $_SESSION["email"] = $email;
         $_SESSION["username"] = $username;
-        // update the user variable
-        $user = $_SESSION["username"];
+        $_SESSION["type"] = $type;
+        $_SESSION["account_id"] = $signin["account"]["id"];
+        // // update the user variable
+       $user = $username;
+       $response = $signin["message"];
     }
     else {
         // failed
         $success = false;
+        $response = $signin["message"];
     }
 }
 
@@ -46,12 +54,14 @@ if( $_SERVER["REQUEST_METHOD"] == "POST" ) {
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment( $loader );
 // load the template into memory
-$template = $twig -> load('signup.html.twig');
+$template = $twig -> load('signin.html.twig');
 // add some variables for twig to render
 echo $template -> render([
     'title' => $title,
     'message' => $message,
     'success' => $success,
-    'user' => $user
+    'user' => $user,
+    'response' => $response,
+    'type' => $type
 ]);
 ?>
--- src/Account.php
+++ src/Account.php
@@ -1,14 +1,18 @@
 <?php
+
 namespace Sc\Webproject;
 
 use Exception;
 use Sc\Webproject\Database;
 
-class Account extends Database {
+class Account extends Database
+{
+    public $response = array();
+
     public function __construct()
     {
         // initialise the database connection
-        parent::__construct();       
+        parent::__construct();
     }
 
     public function create(
@@ -17,8 +21,7 @@ class Account extends Database {
         $username,
         $first,
         $last
-        )
-    {
+    ) {
         $query = "
         INSERT INTO Account
         (Email,Password,Username,First,Last,Type)
@@ -26,23 +29,67 @@ class Account extends Database {
         (?,?,?,?,?,1)
         ";
         $hash = password_hash($password, PASSWORD_DEFAULT);
-        $statement = $this -> connection -> prepare($query);
-        $statement -> bind_param("sssss",$email,$hash,$username,$first,$last);
+        $statement = $this->connection->prepare($query);
+        $statement->bind_param("sssss", $email, $hash, $username, $first, $last);
         try {
-            if( !$statement -> execute()) {
-                throw new Exception("Oops! Something went wrong");
-            }
-            else {
+            if (!$statement->execute()) {
+                throw new Exception("Oops! Something is wrong");
+            } else {
                 // account is created
                 return true;
             }
-        }
-        catch( Exception $e) {
+        } catch (Exception $e) {
             // do something
-            echo $e -> getMessage();
+            echo $e->getMessage();
             // failure to create account
             return false;
         }
     }
+
+    public function login($email, $password)
+    {
+        // find the user in the database
+        $query = "
+            SELECT 
+            id,
+            Email,
+            Username,
+            Password,
+            First,
+            Last,
+            Type
+            FROM Account
+            WHERE 
+            Email = ?
+            AND
+            Active = 1
+        ";
+        $statement = $this->connection->prepare($query);
+        $statement->bind_param("s", $email);
+        try {
+            if (!$statement->execute()) {
+                throw new Exception("database error");
+            }
+            // check if account exists
+            $result = $statement->get_result();
+            if ($result->num_rows == 0) {
+                throw new Exception("Account is inactive or does not exist");
+            }
+            // check password
+            $account = $result->fetch_assoc();
+            if (!password_verify($password, $account["Password"])) {
+                throw new Exception("Invalid credentials");
+            } else {
+                // password matches
+                $this->response["success"] = true;
+                $this->response["message"] = "Login successful";
+                $this->response["account"] = $account;
+            }
+        } catch (Exception $e) {
+            $this->response["success"] = false;
+            $this->response["message"] = $e->getMessage();
+        }
+
+        return $this->response;
+    }
 }
-?>
